ad36c86ed91fe99f1e290e93ce7b87b9991e80e1
Aadarsh Prem <101085150+aadarshprem82@users.noreply.github.com>
2022-09-08 10:13:35 +0530

    Create first.php

---
+++ first.php
@@ -0,0 +1,11 @@
+<?php
+//WAP to print hello message
+#WAP to print hello message
+echo "<h1> <font color = 'red'><body bgcolor = 'lightsalmon'> Welcome</font></h1>";
+echo "<br><hr width='400px', color = 'black'><p style='color:green;'>Hello World</p>";
+echo "<br> Bye<br><br>"; 
+echo "<h5>Aadarsh<font color = 'pink'> Prem</font></h5></body>";
+$first = 12;
+$last = 13;
+echo $first+$last
+?>
