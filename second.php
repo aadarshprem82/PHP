8cdebfb188ef06f7e46a117afe56b3e601613435
Aadarsh Prem <101085150+aadarshprem82@users.noreply.github.com>
2022-09-08 10:35:18 +0530

    Create second.php

---
+++ second.php
@@ -0,0 +1,10 @@
+<?php
+	//How to define variable in pHp
+    $name = "Aadarsh";
+    $last = "Prem";
+    ECHO "Your name is <font color='salmon'>$name $last.<br>";
+    $c="3.5";
+    $d = "4.5";
+    $e = $c+$d;
+    echo $e;
+?>
