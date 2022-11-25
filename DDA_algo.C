0b48fdfd7c087003ff6033819cdff490b13f17a6
Aadarsh Prem <101085150+aadarshprem82@users.noreply.github.com>
2022-11-25 10:13:35 +0530

    Add files via upload

---
+++ DDA_algo.C
@@ -0,0 +1,33 @@
+#include<graphics.h>
+#include<conio.h>
+void main(){
+  int gh = DETECT, gm;
+  int x1,y1,x2,y2,dx,dy,constr,p;
+  initgraph(&gh, &gm, "C:\\TC\\BGI");
+  printf("Enter the value of X1 : ");
+  scanf("%d",&x1);
+  printf("Enter the value of Y1 : ");
+  scanf("%d",&y1);
+  printf("Enter the value of X2 : ");
+  scanf("%d",&x2);
+  printf("Enter the value of Y2 : ");
+  scanf("%d",&y2);
+  dx = x2-x1;
+  dy = y2-y1;
+  constr = dx;
+  p = 2*dy - dx;
+  while(constr>0){
+    putpixel(x1,y1,2);
+    if(p<0){
+      x1 += 1;
+      p = p + 2*dy;
+    }
+    else{
+      x1 += 1;
+      y1 += 1;
+      p = p + 2*(dy-dx);
+    }
+    constr--;
+  }
+  getch();
+}
