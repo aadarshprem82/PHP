b1dfd428e08156a0fe24c8a1825f9e5d7efb48e3
Aadarsh Prem <101085150+aadarshprem82@users.noreply.github.com>
2023-01-05 13:22:23 +0530

    Add files via upload

--- Start.py
+++ Start.py
@@ -1,5 +1,29 @@
 import streamlit as st
+import math
+import random
+import smtplib
 
+OTP = ""
+def OTP_generation(user_name, pass_word):
+        #OTP generation
+        global OTP
+        dig = "0123456789"
+        for i in range(6):
+            OTP += dig[math.floor(random.random()*10)]
+            
+        gen_OTP = "Welcome to \"Title\" \nPlease verify your,\nEmail Id : " + username + "\nPassword : " + pass_word + " \nwith this OTP : " + OTP + "."
+        msg = gen_OTP
+        smtp = smtplib.SMTP('smtp.gmail.com',587)
+        smtp.starttls()
+        smtp.login("aadarshprem82@gmail.com","icyekhmzqjncxpqd")
+        smtp.sendmail('Aadarsh',user_email,msg)
+        
+def verification(temp):
+    global OTP
+    if temp == OTP:
+        return True
+    else:
+        return False
 def main():
     st.title("Page")
     menu = ["Home", "Login", "SignUp"]
@@ -8,12 +32,22 @@ def main():
         st.subheader("Home")
     elif choice == "Login":
         st.subheader("Login Here")
-        username = st.sidebar.text_input("User Name")
-        passwrd = st.sidebar.text_input("Password", type="password")
-        if st.sidebar.button("Login"):
+        username = st.text_input("Email ID")
+        passwrd = st.text_input("Password", type="password")
+        if st.button("Login"):
             st.success("Logged in as {}".format(username))
         
     elif choice == "SignUp":
         st.subheader("Create New Account")
+        username = st.text_input("Email ID")
+        passwrd = st.text_input("Password", type="password")
+        if st.button("Sign Up"):
+            OTP_generation(username)
+            get_otp = st.text_input("OTP")
+            if verifcation(get_otp):
+                st.success("Verified Successfully")
+            else:
+                st.warning("Some Error")
+                
 
 main()
